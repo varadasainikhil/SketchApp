246796994be99bdf9cd624160d2d1a82dc39d8d5
Sai Nikhil Varada <varadasainikhil@gmail.com>
2023-03-15 16:18:47 +0530

    Complete SketchApp

---
+++ main.py
@@ -0,0 +1,34 @@
+from turtle import Turtle, Screen
+
+turtle = Turtle()
+screen = Screen()
+
+
+def move_forward():
+    turtle.forward(10)
+
+
+def move_backward():
+    turtle.back(10)
+
+
+def move_right():
+    turtle.right(30)
+
+
+def move_left():
+    turtle.left(30)
+
+
+def clear():
+    turtle.clear()
+
+
+screen.listen()
+screen.onkey(move_forward, "w")
+screen.onkey(move_backward, "s")
+screen.onkey(move_left, "a")
+screen.onkey(move_right, "d")
+screen.onkey(clear, "c")
+
+screen.exitonclick()
